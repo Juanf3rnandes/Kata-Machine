c47ffb64198b5fea8589af4946e70c8bf32091b1
juan_f3rnandes <juanpereira@hubert.com.br>
2024-07-27 20:57:11 -0300

    fix(Queue): update

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,17 @@
 export default function bubble_sort(arr: number[]): void {
+    let aux = 0;
+    let swap: boolean;
 
+    do {
+        swap = false;
+        for (let i = 0; i < arr.length - 1 - aux; i++) {
+            if (arr[i] > arr[i + 1]) {
+                const temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swap = true;
+            }
+        }
+        aux++;
+    } while (swap);
 }
--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,50 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
+    private head?: Node<T>;
+    private tail?: Node<T>;
     public length: number;
 
-    
-
     constructor() {
+        this.head = undefined;
+        this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
+        this.tail.next = node;
+        this.tail = node;
+    }
 
-}
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
+        const value = this.head.value;
+        this.head = this.head.next;
 
-}
+        if (!this.head) {
+            this.tail = undefined;
+        } else {
+            return value;
+        }
+    }
+
     peek(): T | undefined {
-
-}
+        if (!this.head) {
+            return undefined;
+        } else {
+            return this.head.value;
+        }
+    }
 }
