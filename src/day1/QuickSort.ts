0284acfb9f0ef95c5fabd452b0b1cf918e5823b4
juan_f3rnandes <juanpereira@hubert.com.br>
2024-09-17 22:55:56 -0300

    quicksort solution

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,12 @@
-export default function quick_sort(arr: number[]): void {
+export default function quick_sort(arr: number[]): number[] {
+    if (arr.length < 2) {
+        return arr;
+    }
 
+    const pivot = arr[0];
+
+    let left = arr.slice(1).filter((x) => x <= pivot);
+    let right = arr.slice(1).filter((x) => x > pivot);
+
+    return [...quick_sort(left), pivot, ...quick_sort(right)];
 }
