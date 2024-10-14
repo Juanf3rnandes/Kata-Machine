0018ab91516e204d56b7f732666c9cd6626de367
juan_f3rnandes <juanpereira@hubert.com.br>
2024-10-14 19:47:40 -0300

    compare and BST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,22 @@
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+    if (!curr) {
+        return false;
+    }
+
+    if (curr.value === needle) {
+        return true;
+    }
+
+    if (curr.value < needle) {
+        return search(curr.right, needle);
+    }
+
+    if (curr.value > needle) {
+        return search(curr.left, needle);
+    }
+    return false;
+}
+
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-    return true;
+    return search(head, needle);
 }
